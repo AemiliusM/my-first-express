ea26481e40df055b52716f78810f601daa0f24b9
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-08-10 15:00:55 -0700

    filling out data

--- data.js
+++ data.js
@@ -1,18 +1,28 @@
 const data = [
     {
-
+        id: 1,
+        name: 'Rich',
+        desc: "Having so much money I don't know what to do with it"
     },
     {
-
+        id: 2,
+        name: 'Fly',
+        desc: "To be able to fly"
     },
     {
-
+        id: 3,
+        name: 'Eternal-life',
+        desc: "Live forever"
     },
     {
-
+        id: 4,
+        name: 'Make-Robots',
+        desc:"be able to build robots anytime I want"
     }, 
     {
-
+        id: 5,
+        name: 'Healer',
+        desc: "Be able to heal anything"
     }
 ];
 
