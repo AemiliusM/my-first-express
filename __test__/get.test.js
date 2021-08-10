b4b48b5a941f20b59a76a02ddd2cca270d56731c
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-08-10 15:17:25 -0700

    finished

--- __test__/get.test.js
+++ __test__/get.test.js
@@ -4,9 +4,15 @@ const request = supertest(app);
 
 const data = require('../data.js');
 
-it('gets the dogs endpoint', async () => {
-    const response = await request.get('/puppies');
+it('gets the dreams endpoint', async () => {
+    const response = await request.get('/dreams');
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(data);
+});
+
+it('gets the dream show endpoint' , async () => {
+    const response = await request.get('/dreams/1');
+    expect(response.status).tobe(200);
+    expect(response.body).toEqual(data[0]);
 });
